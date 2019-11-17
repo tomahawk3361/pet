f6492307dd21013467712a8c5c6068e9acf4a1fd
tomahawk3361 <56312649+tomahawk3361@users.noreply.github.com>
2019-11-18 00:17:08 +0300

    Add files via upload

---
+++ lib/chartpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Center(
+        child: new Text('Chart Page',
+        style: new TextStyle(fontSize: 25.0, color: Colors.teal),
+      ),
+      ),
+    );
+  }
+}
